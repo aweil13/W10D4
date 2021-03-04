294833e315fb7d348f5a30f055637f79f596c40e
alejandro <alejandroweil13@gmail.com>
2021-03-04 14:12:04 -0500

    setting up project

---
+++ todos/frontend/reducers/todos_reducer.js
@@ -0,0 +1,12 @@
+import * as actions from "./actions/todo_actions";
+
+
+
+const todosReducer = (state = {}, action) => {
+        switch (action.type){
+            default:
+                return state;
+        }
+}
+
+export default todosReducer;
---
+++ todos/frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Hello");
+    const root = document.getElementById("root");
+    ReactDOM.render(root);  
+});
