783e4c5151be67a2ae951f71f66fe614b50da6ba
alejandro <alejandroweil13@gmail.com>
2021-03-04 16:48:25 -0500

    Finished adding todos

--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import configureStore from "./store/store";
-
+import {receiveTodo, receiveTodos} from "./actions/todo_actions"
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Hello");
     const root = document.getElementById("root");
-    ReactDOM.render(root);
+    // ReactDOM.render(root);
     window.store = configureStore();
+    window.receiveTodo = receiveTodo;
+    window.receiveTodos = receiveTodos;
 });
